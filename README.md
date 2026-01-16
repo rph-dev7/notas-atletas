d5d9acb48b8e5c5856e2cae7ae28cae8c5cb61fe
rph-dev7 <raphael.alves.lg@gmail.com>
2026-01-16 17:10:29 +0000

    Projeto notas-atletas finalizado

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # notas-atletas
 Projeto teste git de Devstart-Senai
+
+## Descrição
+
+Aplicação em JavaScript que calcula a média válida descartando a maior e a menor nota e exibe o resultado no console.
+
+- Como funciona
+
+* Cada atleta possui 5 notas
+* A maior e a menor nota são descartadas
+* A média é calculada com as 3 notas restantes
+
+- Como executar
+
+! Opção 1 – Terminal (Node.js)
+
+1. Instale o Node.js
+2. No terminal, acesse a pasta do projeto
+3. Execute:
+
+```
+node notas-atletas.js
+```
+
+4. O resultado aparecerá no terminal via `console.log`
+
+
+
+## Testes
+
+Use `console.log()` durante o desenvolvimento para verificar valores e etapas do cálculo.
---
+++ notas-atletas.js
@@ -0,0 +1,40 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+function calcMedia(atletas){
+    for (let i=0; i < atletas.length;i++){
+      let notasOrdenadas = atletas[i].notas.sort((a, b) => a-b);
+
+      let notasComputadas = notasOrdenadas.slice(1, 4);
+
+      let soma = 0;
+      notasComputadas.forEach(nota => {
+      soma += nota;
+    });
+
+      let media =  soma / notasComputadas.length;
+
+    console.log(`Atleta: ${atletas[i].nome}`);
+    console.log(`Notas Obtidas: ${notasOrdenadas.join(",")}`);
+    console.log(`Média Válida: ${media}\n`);
+
+  }
+}
+
+calcMedia(atletas);
